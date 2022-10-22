649ca9e34f099e2d704a0b9ca2913c65b52c2467
PiesArentSquare <jjhh12@outlook.com>
2022-10-22 18:06:40 -0400

    added 10s

--- card.js
+++ card.js
@@ -1,4 +1,4 @@
-export const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K']
+export const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
 const suits = {
     spades: ['♠️', 'black'],
