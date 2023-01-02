d8f43968cc3242df743e9d2566d8c3ecab6fc819
PiesArentSquare <jjhh12@outlook.com>
2023-01-02 03:47:52 -0500

    added card transitions

--- game.js
+++ game.js
@@ -3,14 +3,14 @@ import { slot_t, slot_type, setup_slots } from './slot.js'
 
 function deal(deck, tableau_slots, stock) {
     let current = 0
-    
+
     for (let i = 0; i < tableau_slots.length; i++) {
         deck[current].visible = true
         for (let j = i; j < tableau_slots.length; j++) {
             deck[current++].set_slot(tableau_slots[j])
         }
     }
-    
+
     for (; current < deck.length; current++) {
         deck[current].set_slot(stock)
     }
@@ -24,6 +24,7 @@ const discard_slot = new slot_t(document.querySelector('.discard > .card-slot'),
 
 let selected_card = undefined
 let deck = create_deck(handle_select)
+let locked_from_card_moving = false;
 
 function start() {
     shuffle(deck)
@@ -31,6 +32,9 @@ function start() {
 }
 
 function handle_select(card_or_slot) {
+    if (locked_from_card_moving)
+        return
+
     if (card_or_slot instanceof card_t)
         if (selected_card)
             set_card_in_slot(card_or_slot.slot, card_or_slot)
@@ -41,10 +45,16 @@ function handle_select(card_or_slot) {
 }
 
 function refill_stock(_card) {
-    for (let i = discard_slot.cards.length - 1; i >= 0; i--) {
+    if (locked_from_card_moving)
+        return
+
+    for (let i = discard_slot.cards.length - 1; i >= 1; i--) {
         discard_slot.cards[i].visible = false
         discard_slot.cards[i].set_slot(stock_slot)
     }
+
+    discard_slot.cards[0].visible = false
+    move_cards(stock_slot, [discard_slot.cards[0]])
 }
 
 function set_selected(card) {
@@ -53,7 +63,7 @@ function set_selected(card) {
             card.selected = false
         }
     }
-    
+
     selected_card = card
 
     if (card) {
@@ -70,7 +80,7 @@ function select_card(card) {
     }
 
     if (card.slot.type === slot_type.stock) {
-        card.set_slot(discard_slot)
+        move_cards(discard_slot, [card])
         card.visible = true
         set_selected(undefined)
         return
@@ -78,7 +88,7 @@ function select_card(card) {
 
     if (!card.visible)
         return
-    
+
     if (selected_card === card)
         set_selected(undefined)
     else
@@ -93,7 +103,7 @@ function can_place_in_slot(card, slot) {
         } else {
             return card.value === 'A'
         }
-    
+
     } else if (slot.type === slot_type.tableau) {
         if (slot.top()) {
             return slot.top().is_red() !== card.is_red()
@@ -104,21 +114,52 @@ function can_place_in_slot(card, slot) {
     return false
 }
 
-function set_card_in_slot(slot, card) {
+async function set_card_in_slot(slot, card) {
     if (!can_place_in_slot(selected_card, slot)) {
         select_card(card)
         return
     }
-        
+
     let old_slot = selected_card.slot
-    for (let card of selected_card.slot.get_card_stack(selected_card)) {
-        card.set_slot(slot)
-    }
+    await move_cards(slot, selected_card.slot.get_card_stack(selected_card))
 
     if (old_slot.type === slot_type.tableau && old_slot.top())
         old_slot.top().visible = true
-    
+
     set_selected(undefined)
+}
+
+const move_slot = new slot_t(document.querySelector('.move-slot > .card-slot'), slot_type.discard, () => { })
+
+async function move_cards(slot, cards) {
+    const base_card = cards[0]
+    const initial_rect = base_card.element.getBoundingClientRect();
+    const initial_x = initial_rect.left
+    const initial_y = initial_rect.top
+    move_slot.element.style.left = `${initial_x}px`
+    move_slot.element.style.top = `${initial_y}px`
+    move_slot.element.classList.add('transition')
+
+    for (let card of cards)
+        card.set_slot(slot)
+
+    const target_rect = base_card.element.getBoundingClientRect();
+    const target_x = target_rect.left
+    const target_y = target_rect.top
+    move_slot.element.style.left = `${target_x}px`
+    move_slot.element.style.top = `${target_y}px`
+
+    locked_from_card_moving = true
+    for (let card of cards)
+        card.set_slot(move_slot)
+
+    await new Promise(r => setTimeout(r, 150))
+    move_slot.element.classList.remove('transition')
+
+    for (let card of cards)
+        card.set_slot(slot)
+    locked_from_card_moving = false
+
 }
 
 start()
